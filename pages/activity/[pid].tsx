c6183ccb21b7170869d404cc4049f89dba4867db
murraycopps <murraycopps@gmail.com>
2023-01-23 07:39:58 -0500

    added defaults for when no value is present

--- pages/activity/[pid].tsx
+++ pages/activity/[pid].tsx
@@ -39,9 +39,7 @@ export default function ActivityPage({ pid }: { pid: string }) {
         <section className="flex flex-col text-white single-run-field-sizing rounded-lg bg-gray-700 py-8 gap-16 p-2">
           <div className="grid sm:grid-cols-3 gap-4">
             <div className="grid grid-rows-2 items-center">
-              <h1 className="text-center text-3xl">
-                {activity.name}
-              </h1>
+              <h1 className="text-center text-3xl">{activity.name}</h1>
               <h2 className="text-center text-md">
                 {new Date(activity.start_date_local).toLocaleDateString() +
                   " " +
@@ -73,85 +71,107 @@ export default function ActivityPage({ pid }: { pid: string }) {
               </div>
             </div>
           </div>
-          <div className="grid sm:grid-cols-3 grid-cols-2 gap-4 gap-y-16">
-            <div className="flex flex-col items-center justify-start gap-4">
-              <FontAwesomeIcon icon={faHeart} className="w-12 h-12" />{" "}
-              <div className="grid grid-cols-2 w-full">
-                <div className="grid grid-cols-1 gap-2">
-                  <p className="text-center text-md">Average</p>
-                  <p className="text-center text-xl">
-                    {Math.round(activity.average_heartrate * 100) / 100 || '--'}
-                  </p>
-                  <p className="text-center text-sm">BPM</p>
+          {activity.average_heartrate ||
+          activity.max_speed ||
+          activity.average_cadence ? (
+            <div className="grid sm:grid-cols-3 grid-cols-2 gap-4 gap-y-16">
+              <div className="flex flex-col items-center justify-start gap-4">
+                <FontAwesomeIcon icon={faHeart} className="w-12 h-12" />{" "}
+                <div className="grid grid-cols-2 w-full">
+                  <div className="grid grid-cols-1 gap-2">
+                    <p className="text-center text-md">Average</p>
+                    <p className="text-center text-xl">
+                      {Math.round(activity.average_heartrate * 100) / 100 ||
+                        "--"}
+                    </p>
+                    <p className="text-center text-sm">BPM</p>
+                  </div>
+                  <div className="grid grid-cols-1 gap-2">
+                    <p className="text-center text-md">Max</p>
+                    <p className="text-center text-xl">
+                      {Math.round(activity.max_heartrate * 100) / 100 || "--"}
+                    </p>
+                    <p className="text-center text-sm">BPM</p>
+                  </div>
                 </div>
-                <div className="grid grid-cols-1 gap-2">
-                  <p className="text-center text-md">Max</p>
-                  <p className="text-center text-xl">
-                    {Math.round(activity.max_heartrate * 100) / 100 || '--'}
-                  </p>
-                  <p className="text-center text-sm">BPM</p>
+              </div>
+              <div className="flex flex-col items-center justify-start gap-4">
+                <FontAwesomeIcon icon={faGaugeHigh} className="w-12 h-12" />{" "}
+                <div className="grid grid-cols-2 w-full">
+                  <div className="grid grid-cols-1 gap-2">
+                    <p className="text-center text-md">Average</p>
+                    <p className="text-center text-xl">
+                      {activity.average_speed
+                        ? Math.round(activity.average_speed * 100) / 100
+                        : "--"}
+                    </p>
+                    <p className="text-center text-sm">MPH</p>
+                  </div>
+                  <div className="grid grid-cols-1 gap-2">
+                    <p className="text-center text-md">Max</p>
+                    <p className="text-center text-xl">
+                      {activity.max_speed
+                        ? Math.round(activity.max_speed * 100) / 100
+                        : "--"}
+                    </p>
+                    <p className="text-center text-sm">MPH</p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col items-center justify-start gap-4 sm:col-span-1 col-span-2  ">
+                <FontAwesomeIcon icon={faRunning} className="w-12 h-12" />{" "}
+                <div className="grid sm:grid-cols-3 grid-cols-5 w-full">
+                  <div className="sm:hidden"></div>
+                  <div className="grid grid-cols-1 gap-2">
+                    <p className="text-center text-md">Average</p>
+                    <p className="text-center text-xl">
+                      {activity.average_cadence
+                        ? Math.round(activity.average_cadence * 100) / 100
+                        : "--"}
+                    </p>
+                    <p className="text-center text-sm">SPM</p>
+                  </div>
+                  <div className="grid grid-cols-1 gap-2">
+                    <p className="text-center text-md">Total</p>
+                    <p className="text-center text-xl">
+                      {activity.average_cadence
+                        ? Math.round(
+                            (activity.average_cadence * activity.moving_time) /
+                              30
+                          )
+                        : "--"}
+                    </p>
+                    <p className="text-center text-sm">Steps</p>
+                  </div>
+                  <div className="grid grid-cols-1 gap-2">
+                    <p className="text-center text-md">Length</p>
+                    <p className="text-center text-xl">
+                      {activity.average_cadence
+                        ? Math.round(
+                            (activity.average_speed /
+                              (activity.average_cadence / 30)) *
+                              100
+                          ) / 100
+                        : "--"}
+                    </p>
+                    <p className="text-center text-sm">Meters</p>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="flex flex-col items-center justify-start gap-4">
-              <FontAwesomeIcon icon={faGaugeHigh} className="w-12 h-12" />{" "}
-              <div className="grid grid-cols-2 w-full">
-                <div className="grid grid-cols-1 gap-2">
-                  <p className="text-center text-md">Average</p>
-                  <p className="text-center text-xl">
-                    {Math.round(activity.average_speed * 100) / 100}
-                  </p>
-                  <p className="text-center text-sm">MPH</p>
-                </div>
-                <div className="grid grid-cols-1 gap-2">
-                  <p className="text-center text-md">Max</p>
-                  <p className="text-center text-xl">
-                    {Math.round(activity.max_speed * 100) / 100}
-                  </p>
-                  <p className="text-center text-sm">MPH</p>
-                </div>
-              </div>
+          ) : (
+            <div className="flex flex-col items-center justify-center gap-4">
+              <p className="text-2xl">No Data</p>
             </div>
-            <div className="flex flex-col items-center justify-start gap-4 sm:col-span-1 col-span-2  ">
-              <FontAwesomeIcon icon={faRunning} className="w-12 h-12" />{" "}
-              <div className="grid sm:grid-cols-3 grid-cols-5 w-full">
-                <div className="sm:hidden"></div>
-                <div className="grid grid-cols-1 gap-2">
-                  <p className="text-center text-md">Average</p>
-                  <p className="text-center text-xl">
-                    {Math.round(activity.average_cadence * 100) / 100}
-                  </p>
-                  <p className="text-center text-sm">SPM</p>
-                </div>
-                <div className="grid grid-cols-1 gap-2">
-                  <p className="text-center text-md">Total</p>
-                  <p className="text-center text-xl">
-                    {Math.round(
-                      (activity.average_cadence * activity.moving_time) / 30
-                    )}
-                  </p>
-                  <p className="text-center text-sm">Steps</p>
-                </div>
-                <div className="grid grid-cols-1 gap-2">
-                  <p className="text-center text-md">Length</p>
-                  <p className="text-center text-xl">
-                    {Math.round(
-                      (activity.average_speed /
-                        (activity.average_cadence / 30)) *
-                        100
-                    ) / 100}
-                  </p>
-                  <p className="text-center text-sm">Meters</p>
-                </div>
-              </div>
-            </div>
-          </div>
+          )}
         </section>
-      ):
-      (
+      ) : (
         <section className="flex flex-col items-center justify-center gap-4 text-white">
           <p className="text-2xl">Loading...</p>
-          <FontAwesomeIcon icon={faSpinner} className="w-12 h-12 animate-spin" />
+          <FontAwesomeIcon
+            icon={faSpinner}
+            className="w-12 h-12 animate-spin"
+          />
         </section>
       )}
       <button
@@ -162,8 +182,6 @@ export default function ActivityPage({ pid }: { pid: string }) {
       >
         <FontAwesomeIcon icon={faArrowLeft} className="w-6 h-6" />
       </button>
-      
-      
     </main>
   );
 }

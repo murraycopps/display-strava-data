13b03155ca530d5f31752896a49630b33f79b01a
murraycopps <murraycopps@gmail.com>
2023-01-31 08:51:30 -0500

    somethign

--- pages/goals/index.tsx
+++ pages/goals/index.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import GoalCard from "../../components/GoalCard";
 import LoginData from "../../scripts/LoginData";
 import { Goal } from "../../scripts/types";
 
 export default function Goals({ url }: { url: string }) {
   const router = useRouter();
+
+  const [displayCompleted, setDisplayCompleted] = useState(false);
 
   useEffect(() => {
     if (!LoginData.isLoggedIn()) {
@@ -20,24 +22,36 @@ export default function Goals({ url }: { url: string }) {
   }, [LoginData.isLoggedIn(), router]);
 
   return (
-    <div className="flex flex-col items-center gap-4 font-sans text-white bg-gray-800">
+    <main className="flex flex-col items-center gap-4 font-sans text-white bg-gray-800">
       <div className="px-16 py-4 m-4 text-center bg-gray-700 rounded-lg">
         <h1 className="text-3xl">Goals</h1>
       </div>
-      <Link
-        href="/goals/create"
-        className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
-      >
-        Create a new goal
-      </Link>
-      {LoginData.getGoals().length
-        ? LoginData.getGoals().map((goal: Goal) => (
-            <Link href={`/goals/${goal.id}`} key={goal.id}>
-              <GoalCard goal={goal} />
-            </Link>
-          ))
+      <div className="flex flex-row gap-4">
+        <button
+          className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
+          onClick={() => setDisplayCompleted(!displayCompleted)}
+        >
+          {displayCompleted ? "Hide" : "Show"} completed goals
+        </button>
+        <Link
+          href="/goals/create"
+          className="px-4 py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600"
+        >
+          Create a new goal
+        </Link>
+      </div>
+      {LoginData.getGoals().filter(
+        (goal: Goal) => displayCompleted || !goal.completed
+      ).length > 0
+        ? LoginData.getGoals()
+            .filter((goal: Goal) => displayCompleted || !goal.completed)
+            .map((goal: Goal) => (
+              <Link href={`/goals/${goal.id}`} key={goal.id}>
+                <GoalCard goal={goal} />
+              </Link>
+            ))
         : "No Goals"}
-    </div>
+    </main>
   );
 }
 
